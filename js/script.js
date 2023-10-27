2b86c4e595a2131a5ce0417cba95cfc4974b9350
Oleksandr Ohar <122322748+AlexOhar@users.noreply.github.com>
2023-10-28 02:35:15 +0300

    add files portfolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const cardSkyStudy = document.querySelector('.cardSkyStudy'),
+      cardCottonBro = document.querySelector('.cardCottonBro'),
+      cardTetris = document.querySelector('.cardTetris');
+
+cardSkyStudy.addEventListener('click', () => {
+    window.location.href = `https://alexohar.github.io/skystudy/`;
+});
+
+cardCottonBro.addEventListener('click', () => {
+    window.location.href = `https://alexohar.github.io/cotton-bro/`;
+});
+
+cardTetris.addEventListener('click', () => {
+    window.location.href = `https://alexohar.github.io/tetris/`;
+});
