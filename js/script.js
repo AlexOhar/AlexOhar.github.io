4317972e661d283d431fdf5715705af6038153b8
Oleksandr Ohar <122322748+AlexOhar@users.noreply.github.com>
2023-10-29 00:39:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-const cardSkyStudy = document.querySelector('.portfolio_cards_card_cardSkyStudy'),
-      cardCottonBro = document.querySelector('.portfolio_cards_card_cardCottonBro'),
-      cardTetris = document.querySelector('.portfolio_cards_card_cardTetris');
-
-cardSkyStudy.addEventListener('click', () => {
-    window.location.href = `https://alexohar.github.io/skystudy/`;
-});
-
-cardCottonBro.addEventListener('click', () => {
-    window.location.href = `https://alexohar.github.io/cotton-bro/`;
-});
-
-cardTetris.addEventListener('click', () => {
-    window.location.href = `https://alexohar.github.io/tetris/`;
+document.addEventListener('DOMContentLoaded', () => {
+    const projectCards = document.querySelectorAll('.portfolio_cards_card');
+    projectCards.forEach(card => {
+        if (card.getAttribute('data-link')) {
+            card.addEventListener('click', () => {
+                window.location.href = `${card.getAttribute('data-link')}`;
+            });
+        };
+        if (card.getAttribute('data-bg-img')) {
+            card.style.backgroundImage = `url(${card.getAttribute('data-bg-img')})`;
+        };
+    });
 });
