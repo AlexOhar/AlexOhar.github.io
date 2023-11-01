6cee9443932276a01326e70069ad84821ca9fda3
Oleksandr Ohar <122322748+AlexOhar@users.noreply.github.com>
2023-11-02 00:35:56 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,26 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
             card.style.backgroundImage = `url(${card.getAttribute('data-bg-img')})`;
         };
     });
-});
-
-// loading.style.display = 'none';
-
-window.addEventListener('load', function() {
-    const loading = document.getElementById('loading');
-    setTimeout(() => {
-        loading.style.display = 'none';
-    }, 10000);
-    
-    const titleOne = document.querySelector('.title');
-    const titleTwo = document.querySelector('.titleTwo');
-    setTimeout(function() {
-        titleOne.style.display = 'none';
-    }, 7000);
-    setTimeout(function() {
-        titleOne.style.display = 'none';
-        titleTwo.style.display = 'block';
-        titleTwo.style.animation = "printed-text 4s steps(25), flashin-border .75s step-start infinite";
-    }, 4000);
 
     const linksBtnOpen = document.querySelector('.portfolio_links_btn'),
             linksMenu = document.querySelector('.portfolio_links'),
@@ -58,25 +38,39 @@ window.addEventListener('load', function() {
     });
 });
 
-// const linksBtnOpen = document.querySelector('.portfolio_links_btn'),
-//       linksMenu = document.querySelector('.portfolio_links'),
-//       linkLinkedIn = document.querySelector('.portfolio_links_big'),
-//       linkGitHub = document.querySelector('.portfolio_links_big_middle'),
-//       linkTelegram = document.querySelector('.portfolio_links_big_middle_small');
+loading.style.display = 'none';
 
-// linksBtnOpen.addEventListener('click', () => {
-//     linksMenu.classList.toggle('portfolio_links_openLinks');
-//     linksBtnOpen.classList.toggle('hover-effect');
-// });
+window.addEventListener('load', function() {
+    const loading = document.getElementById('loading');
+    setTimeout(() => {
+        loading.style.display = 'none';
+    }, 10000);
+    
+    const titleOne = document.querySelector('.title');
+    const titleTwo = document.querySelector('.titleTwo');
+    setTimeout(function() {
+        titleOne.style.display = 'none';
+    }, 7000);
+    setTimeout(function() {
+        titleOne.style.display = 'none';
+        titleTwo.style.display = 'block';
+        titleTwo.style.animation = "printed-text 4s steps(25), flashin-border .75s step-start infinite";
+    }, 4000);
 
-// linkLinkedIn.addEventListener('click', () => {
-//     window.location.href = 'https://www.linkedin.com/in/oleksandr-ohar-70093b229';
-// });
+    
+});
 
-// linkGitHub.addEventListener('click', () => {
-//     window.location.href = 'https://github.com/AlexOhar';
-// });
+// вариант слипкими карточками 
+// window.addEventListener('scroll', updateCardPositions);
 
-// linkTelegram.addEventListener('click', () => {
-//     window.location.href = 'https://t.me/shin_shila';
-// });
+// function updateCardPositions() {
+//     if (window.innerWidth >= 575) return;
+//     const cards = document.querySelectorAll('.portfolio_cards_card');
+//     cards.forEach((card, i) => {
+//         card.style.top = `${30 + 30 * i}px`;
+//         // if ( i != 0) {
+//         //     card.style.top = `${88 + 40 * i}px`;
+//         // }
+//     })
+// };
+
